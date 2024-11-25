b3bfc692f73106d7dff71c8fd6bba8560f3fc82e
wolfiiy <wolfiy@protonmail.ch>
2024-11-25 10:51:36 +0100

    Créé un modèle pour les utilisateurs

--- src/models/UserModel.php
+++ src/models/UserModel.php
@@ -1,7 +1,31 @@
 <?php
 
-include('DatabaseModel.php');
+/**
+ * Authors: Valentin Pignat, Sébastien Tille
+ * Date: November 25th, 2024
+ */
 
+include_once('DatabaseModel.php');
+
+/**
+ * This class holds database queries relative to users.
+ */
 Class UserModel extends DatabaseModel {
     
+    /**
+     * Given an ID, will attempt to get the corresponding user from the 
+     * database. This method is safe to use with user-provided inputs.
+     * @param int $id Unique identifier of the user. Used as a foreign key
+     * in the t_book table.
+     * @return array|null An array that contains information about the 
+     * user (username, account creation date) if it has been found and false
+     * otherwise.
+     */
+    public function getUserById(int $id) {
+        $sql = "select * from t_user where user_id = :user_id";
+        $binds = array(':user_id' => $id);
+        $query = $this->queryPrepareExecute($sql, $binds);
+
+        return $this->formatData($query);
+    }
 }
