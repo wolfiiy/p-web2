7d304a19aaad064e16cbee3917d03f84d2087728
Valentin Pignat <pe41oyl@eduvaud.ch>
2024-11-26 10:00:08 +0100

    Ajout de BookPreview pour la récupération des infos d'un livre

--- src/views/indexView.php
+++ src/views/indexView.php
@@ -7,11 +7,12 @@
         Liste des dernières oeuvres
         </p>
 <?php
+
     foreach($latestBooks as $book){
         echo "<div class = 'book-preview'>";
         echo "<img src='https://developers.elementor.com/docs/assets/img/elementor-placeholder-image.png'>";
-        echo "<div>". $book["title"] . '<br>'. $authors[$book["book_id"]]. "<br>". $categories[$book["book_id"]]."</div>";
-        echo "<div>". $ratings[$book["book_id"]] . '<br>'. $users[$book["book_id"]]. "<br>". "<a href = 'index.php?controller=book&action=detail&id=". $book["book_id"] ."'>Détails</a>"."</div>";
+        echo "<div>". $book["title"] . '<br>'. $book["author_name"]. "<br>". $book["category_name"]."</div>";
+        echo "<div>". $book["average_rating"] . '<br>'. $book["username_name"]. "<br>". "<a href = 'index.php?controller=book&action=detail&id=". $book["book_id"] ."'>Détails</a>"."</div>";
 
         echo "</div>";
     }
