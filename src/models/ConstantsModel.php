5b920a0dcd43222ce2dfa39534844eecb99cee6f
AbigaelP <150002397+AbigaelP@users.noreply.github.com>
2024-12-17 10:06:19 +0100

    Ajout constante d'erreur

--- src/models/ConstantsModel.php
+++ src/models/ConstantsModel.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Author: STE
  * Date: October 8th, 2024
@@ -7,20 +8,25 @@
 /**
  * Holds application-wide constants.
  */
-class Constants {
+class Constants
+{
     /**
-     * Error message to display when the last name is missing in a form.
+     * Error message to display when the field is missing in a form.
      */
     const ERROR_MISSING_LAST_NAME = "Veuillez entrer un nom de famille.";
 
     /**
-     * Error message to display when the first name is missing in a form.
+     * Error message to display when the text exceed the authorized size.
      */
-    const ERROR_MISSING_FIRST_NAME = "Veuillez entrer un prénom.";
+    const ERROR_LENGTH  = "Le champ doit avoir un nombre de caractères entre 2 et 50";
+
+    /**
+     * Error message to display when the field is not a url.
+     */
+    const ERROR_URL = "L'url n'est pas valide";
 
     /**
      * Message to display when no ratings have been given to a book.
      */
     const NO_RATING = "Aucune note";
 }
-?>
