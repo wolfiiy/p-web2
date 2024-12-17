50801a4f4b9cdd4d00cb4ca6721bf72cda0ffcdd
wolfiiy <wolfiy@protonmail.ch>
2024-12-17 09:35:24 +0100

    Mise en page signin

--- src/views/login.php
+++ src/views/login.php
@@ -1,18 +1,26 @@
 <main>
 <h1>Login Page</h1>
-<form enctype = "multipart/form-data" action="index.php?controller=user&action=login" method="post" id="bookForm">
-
-<div class="label-input">    
-    <label for="userAttemp">Username</label>
-    <input type="text" name="userAttemp" id="userAttemp">
+<div class="submission-container">
+    <form enctype = "multipart/form-data" 
+          action="index.php?controller=user&action=login" 
+          method="post" 
+          id="bookForm"
+          class="large-form wrap-col">
+    
+        <div class="md-outlined-input large on-surface-container">    
+            <input type="text" name="userAttemp" id="userAttemp" placeholder=" ">
+            <label for="userAttemp">Nom d'utilisateur</label>
+        </div>
+    
+        <div class="md-outlined-input large on-surface-container">    
+            <input type="password" name="passAttemp" id="passAttemp" placeholder=" ">
+            <label for="passAttemp">Mot de passe</label>
+        </div>
+    
+        <div>
+            <input type="submit" value="Login" class="md-button primary">
+            <button type="button">Mot de passe oublié</button>
+        </div>
+    </form>
 </div>
-<div class="label-input">    
-    <label for="passAttemp">Password</label>
-    <input type="text" name="passAttemp" id="passAttemp">
-</div>
-<div>
-    <input type = "submit" value="Login">
-</div>
-
-</form>
 </main>
