ded2c257deac41f85c0e172b363dd4e3204d9269
wolfiy <wolfiy@protonmail.ch>
2025-01-03 19:42:39 +0100

    Conversion des styles en sass

--- src/views/partials/head.php
+++ src/views/partials/head.php
@@ -4,15 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php echo isset($pageTitle) ? $pageTitle : "P_Web2"; ?></title>
-    <link rel="stylesheet" href="assets/css/main.css">
+    <link rel="stylesheet" href="assets/css/styles.css">
     <script src="assets/js/main.js"></script>
-    <script type="importmap">
-        {
-        "imports": {
-            "@material/web/": "https://esm.run/@material/web/"
-        }
-        }
-    </script>
     <script type="module">
         import '@material/web/all.js';
         import {styles as typescaleStyles} from '@material/web/typography/md-typescale-styles.js';
