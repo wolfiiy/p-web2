67490240970b7c28c8e68931b3cf68d416ca2bb0
AbigaelP <150002397+AbigaelP@users.noreply.github.com>
2025-01-06 21:19:03 +0100

    changement des affichages des erreurs

--- src/helpers/utils.php
+++ src/helpers/utils.php
@@ -6,11 +6,6 @@
  * Date: December 17th, 2024
  */
 
-// TODO
-// - Turn into a proper static class
-// - Use constants for regex
-// Complete documentation
-
 /**
  * Checks whether the user is logged in by peeking at the session variable.
  * @return bool True if the user is logged in, false otherwise.
@@ -26,103 +21,5 @@ function isUserConnected() {
 function isAdminConnectedUser() {
     if (isUserConnected()) {
         return $_SESSION["is_admin"];
-    }
-}
-
-/**
- * Check name entry
- * @return bool false if not fit into the criteria
- */
-function textController($name) {
-    //vérifier le champs 
-    if (isset($name)) {
-        $name = trim($name);
-
-        if (empty($name))
-            return false;
-        elseif (!preg_match("/^[a-zA-ZÀ-ÿ\s\-]+$/", $name))
-            return false;
-        
-        $name = htmlspecialchars($name);
-        return $name;
-    } else {
-        return false;
-    }
-}
-
-/**
- * Check year entry
- * @return bool false if noot 4 number
- */
-function yearController($year) {
-    if (isset($year)) {
-        $year = trim($year);
-
-        if (empty($year)) {
-            return false;
-        } elseif (!preg_match("/^\d{4}$/", $year)) {
-            return false;
-        }
-        return $year;
-    } else {
-        return false;
-    }
-}
-
-/**
- * Verify that a number is a positive integer.
- * @return int|false The number if it is a positive integer, false otherwise.
- */
-function pageController($number) {
-    if (isset($number)) {
-        $number = trim($number);
-
-        if (empty($number)) {
-            return false;
-        } elseif (!is_numeric($number)) {
-            return false;
-        }
-        return $number;
-    } else {
-        return false;
-    }
-}
-
-/**
- * Checks an URL's validity.
- * @return bool false if is not a url
- */
-function urlController($url) {
-    if (isset($url)) {
-        $url = trim($url);
-
-        if (empty($url)) {
-            return false;
-        } elseif (!preg_match("/(?:http[s]?:\/\/.)?(?:www\.)?[-a-zA-Z0-9@%._\+~#=]{2,256}\.[a-z]{2,6}\b(?:[-a-zA-Z0-9@:%_\+.~#?&\/\/=]*)/", $url)) {
-            return false;
-        }
-        return $url;
-    } else {
-        return false;
-    }
-}
-
-/**
- * Check text entry
- * @return bool false if is special characters
- */
-function resumeController($summary) {
-    if (isset($summary)) {
-        $url = trim($summary);
-
-        if (empty($summary)) {
-            return false;
-        } elseif (!preg_match("/^[a-zA-ZÀ-ÿ0-9\s\-\.,:;()?!']+$/", $summary)) {
-            return false;
-        }
-        $summary = htmlspecialchars($summary);
-        return $summary;
-    } else {
-        return false;
     }
 }
--- src/views/addBook.php
+++ src/views/addBook.php
@@ -6,94 +6,113 @@ Description: Form used to add or modify an entry.
 -->
 
 <main>
-<h1><?=$h1?></h1>
-<div class="submission-container">
-    <form enctype = "multipart/form-data" 
-          action="<?=$actionURL?>" 
-          method="post" 
-          id="bookForm"
-          class="large-form wrap-col">
+    <h1><?= $h1; ?></h1>
+    <div class="submission-container">
+        <form enctype="multipart/form-data"
+            action="<?= $actionURL; ?>"
+            method="post"
+            id="bookForm"
+            class="large-form wrap-col">
 
-        <div class="md-outlined-input on-surface-container">    
-            <input type="text" name="authorFirstName" id="authorFirstName" value="<?=$author["first_name"];?>" placeholder=" ">
-            <label for="authorFirstName">Prénom de l'auteur</label>
-            <?php if (array_key_exists("authorFirstName", $errors)) { echo '<p style="color:red;">' . $errors["authorFirstName"] . '</p>';}?>
-        </div>
+            <div class="md-outlined-input on-surface-container">
+                <input type="text" name="authorFirstName" id="authorFirstName" value="<?= $author["first_name"]; ?>" placeholder=" ">
+                <label for="authorFirstName">Prénom de l'auteur</label>
+            </div>
+                <?php if (array_key_exists("authorFirstName", $errors)) {
+                    echo '<p style="color:red;">' . $errors["authorFirstName"] . '</p>';
+                } ?>
 
-        <div class="md-outlined-input on-surface-container">
-            <input type="text" name="authorLastName" id="authorLastName" value="<?=$author["last_name"];?>" placeholder=" ">
-            <label for="authorLastName">Nom de l'auteur</label>
-            <?php if (array_key_exists("authorLastName", $errors)) { echo '<p style="color:red;">' . $errors["authorLastName"] . '</p>';}?>
-        </div>
-        
-        <div class="md-outlined-input on-surface-container">
-            <input type="text" name="bookTitle" id="bookTitle" value="<?=$book["title"];?>" placeholder=" ">
-            <label for="bookTitle">Titre du livre</label>
-            <?php if (array_key_exists("bookTitle", $errors)) { echo '<p style="color:red;">' . $errors["bookTitle"] . '</p>';}?>
-        </div>
-        
-        <div class="md-outlined-input on-surface-container">
-            <input type="text" name="bookEditor" id="bookEditor"  value="<?=$publisher["name"];?>" placeholder=" ">
-            <label for="bookEditor">Editeur</label>
-            <?php if (array_key_exists("bookEditor", $errors)) { echo '<p style="color:red;">' . $errors["bookEditor"] . '</p>';}?>
+            <div class="md-outlined-input on-surface-container">
+                <input type="text" name="authorLastName" id="authorLastName" value="<?= $author["last_name"]; ?>" placeholder=" ">
+                <label for="authorLastName">Nom de l'auteur</label>
+            </div>
+                <?php if (array_key_exists("authorLastName", $errors)) {
+                    echo '<p style="color:red;">' . $errors["authorLastName"] . '</p>';
+                } ?>
 
-        </div>
-        
-        <div class="label-input">
-        <label for="bookEditionYear">Année d'édition</label>
-            <input class="md-input secondary small" type="date" name="bookEditionYear" id="bookEditionYear" value="<?=$book["release_date"];?>">
-            <label for="bookEditionYear">Année d'édition</label>
-            <?php if (array_key_exists("bookEditionYear", $errors)) { echo '<p style="color:red;">' . $errors["bookEditionYear"] . '</p>';}?>
+            <div class="md-outlined-input on-surface-container">
+                <input type="text" name="bookTitle" id="bookTitle" value="<?= $book["title"]; ?>" placeholder=" ">
+                <label for="bookTitle">Titre du livre</label>
+            </div>
+                <?php if (array_key_exists("bookTitle", $errors)) {
+                    echo '<p style="color:red;">' . $errors["bookTitle"] . '</p>';
+                } ?>
 
-        </div>
-        
-        <div class="md-outlined-input on-surface-container">
-            <input type="number" name="bookPageNb" id="bookPageNb" value="<?=$book["number_of_pages"];?>" placeholder=" ">
-            <label for="bookPageNb">Nombre de page</label>
-            <?php if (array_key_exists("bookPageNb", $errors)) { echo '<p style="color:red;">' . $errors["bookPageNb"] . '</p>';}?>
-        </div>
-        
-        <div class="label-input">
-            <label for="bookGenre">Genre</label>
-            <!-- Categories ($genre) gathered from the BookController -->
-            <select class="md-select secondary" name="bookGenre" id="bookGenre">
-                <?php
-                foreach($genres as $genre){
-                    echo "<option value='". $genre["category_id"] . "'";
-                    if (isset($book["category_fk"])){
-                        if ($genre["category_id"] == $book["category_fk"]){echo "selected";}
+            <div class="md-outlined-input on-surface-container">
+                <input type="text" name="bookEditor" id="bookEditor" value="<?= $publisher["name"]; ?>" placeholder=" ">
+                <label for="bookEditor">Editeur</label>
+            </div>
+                <?php if (array_key_exists("bookEditor", $errors)) {
+                    echo '<p style="color:red;">' . $errors["bookEditor"] . '</p>';
+                } ?>
+
+            <div class="label-input">
+                <label for="bookEditionYear">Année d'édition</label>
+                <input class="md-input secondary small" type="date" name="bookEditionYear" id="bookEditionYear" value="<?= $book["release_date"]; ?>">
+            </div>
+                <?php if (array_key_exists("bookEditionYear", $errors)) {
+                    echo '<p style="color:red;">' . $errors["bookEditionYear"] . '</p>';
+                } ?>
+
+            <div class="md-outlined-input on-surface-container">
+                <input type="number" name="bookPageNb" id="bookPageNb" value="<?= $book["number_of_pages"]; ?>" placeholder=" ">
+                <label for="bookPageNb">Nombre de page</label>
+            </div>
+                <?php if (array_key_exists("bookPageNb", $errors)) {
+                    echo '<p style="color:red;">' . $errors["bookPageNb"] . '</p>';
+                } ?>
+
+            <div class="label-input">
+                <label for="bookGenre">Genre</label>
+                <!-- Categories ($genre) gathered from the BookController -->
+                <select class="md-select secondary" name="bookGenre" id="bookGenre">
+                    <?php
+                    foreach ($genres as $genre) {
+                        echo "<option value='" . $genre["category_id"] . "'";
+                        if (isset($book["category_fk"])) {
+                            if ($genre["category_id"] == $book["category_fk"]) {
+                                echo "selected";
+                            }
+                        }
+                        echo  ">" . $genre["name"] . "</option>";
                     }
-                    echo  ">" . $genre["name"] . "</option>";
-                }
-                ?>
-            </select>
-        </div>
+                    ?>
+                </select>
+            </div>
 
-        <div class="label-input">
-            <label for="coverImage">Image de couverture</label>
-            <input class="md-input secondary large" type="file" name="coverImage" id="coverImage">  
-            <?php if (array_key_exists("coverImage", $errors)) { echo '<p style="color:red;">' . $errors["coverImage"] . '</p>';}?>
-  
-        </div>
-<?php if ($_GET["action"] == "modify"){ echo "<p>Charger un nouveau fichier pour changer l'image</p>";}?> 
-        
-        <div class="md-outlined-input on-surface-container">
-            <input type="text" name="snippetLink" id="snippetLink" value="<?=$book["excerpt"];?>" placeholder=" ">
-            <label for="snippetLink">Lien vers un extrait</label>
-            <?php if (array_key_exists("snippetLink", $errors)) { echo '<p style="color:red;">' . $errors["snippetLink"] . '</p>';}?>
+            <div class="label-input">
+                <label for="coverImage">Image de couverture</label>
+                <input class="md-input secondary large" type="file" name="coverImage" id="coverImage">
+                <?php if (array_key_exists("coverImage", $errors)) {
+                    echo '<p style="color:red;">' . $errors["coverImage"] . '</p>';
+                } ?>
 
-        </div>
-        
-        <div class="md-outlined-textarea on-surface-container">
-            <label for="bookSummary">Résumé</label>
-            <textarea class="md-textarea secondary" name="bookSummary" id="bookSummary" ><?=$book["summary"];?></textarea>
-            <?php if (array_key_exists("bookSummary", $errors)) { echo '<p style="color:red;">' . $errors["bookSummary"] . '</p>';}?>
+            </div>
+            <?php if ($_GET["action"] == "modify") {
+                echo "<p>Charger un nouveau fichier pour changer l'image</p>";
+            } ?>
 
-        </div>
-        <div>
-            <input type = "submit" value="<?php echo $submitButton ?>" class="md-button primary">
-            <button type = "button" onclick="document.getElementById('bookForm').reset();" class="md-button error">Effacer</button>
-        </div>
-    </form>
-</div>
+            <div class="md-outlined-input on-surface-container">
+                <input type="text" name="snippetLink" id="snippetLink" value="<?= $book["excerpt"]; ?>" placeholder=" ">
+                <label for="snippetLink">Lien vers un extrait</label>
+                <?php if (array_key_exists("snippetLink", $errors)) {
+                    echo '<p style="color:red;">' . $errors["snippetLink"] . '</p>';
+                } ?>
+
+            </div>
+
+            <div class="md-outlined-textarea on-surface-container">
+                <label for="bookSummary">Résumé</label>
+                <textarea class="md-textarea secondary" name="bookSummary" id="bookSummary"><?= $book["summary"]; ?></textarea>
+                <?php if (array_key_exists("bookSummary", $errors)) {
+                    echo '<p style="color:red;">' . $errors["bookSummary"] . '</p>';
+                } ?>
+
+            </div>
+            <div>
+                <input type="submit" value="<?php echo $submitButton ?>" class="md-button primary">
+                <button type="button" onclick="document.getElementById('bookForm').reset();" class="md-button error">Effacer</button>
+            </div>
+        </form>
+    </div>
 </main>
