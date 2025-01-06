26876f5458f79699561775b54df8047bd8188541
Valentin Pignat <pignatvalentin@gmail.com>
2025-01-06 20:52:48 +0100

    Traduction et indentation

--- src/views/addBook.php
+++ src/views/addBook.php
@@ -47,15 +47,15 @@ Description: Form used to add or modify an entry.
         
         <div class="label-input">
             <label for="bookGenre">Genre</label>
-            <!-- $genres sont récupéré dans le bookController et passé à cette vue -->
+            <!-- Categories ($genre) gathered from the BookController -->
             <select class="md-select secondary" name="bookGenre" id="bookGenre">
                 <?php
                 foreach($genres as $genre){
                     echo "<option value='". $genre["category_id"] . "'";
-            if (isset($book["category_fk"])){
-                if ($genre["category_id"] == $book["category_fk"]){echo "selected";}
-            }
-            echo  ">" . $genre["name"] . "</option>";
+                    if (isset($book["category_fk"])){
+                        if ($genre["category_id"] == $book["category_fk"]){echo "selected";}
+                    }
+                    echo  ">" . $genre["name"] . "</option>";
                 }
                 ?>
             </select>
